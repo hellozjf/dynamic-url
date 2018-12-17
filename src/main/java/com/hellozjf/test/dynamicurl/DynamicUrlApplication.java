d43bcd978ade358a3bd8cc1109bb65f48751d893
hellozjf <908686171@qq.com>
2018-12-17 11:20:30 +0800

    init

---
+++ src/main/java/com/hellozjf/test/dynamicurl/DynamicUrlApplication.java
@@ -0,0 +1,14 @@
+package com.hellozjf.test.dynamicurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicUrlApplication.class, args);
+	}
+
+}
+
